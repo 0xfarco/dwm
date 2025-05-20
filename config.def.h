050706359b9b00c77964024e3bbda1caba2fb668
0xfarco <0xfarco@gmail.com>
2025-05-20 11:43:30 +0530

    remove firefox from rules

--- config.def.h
+++ config.def.h
@@ -56,7 +56,6 @@ static const Rule rules[] = {
 	 */
 	/* class     					instance		title				tags mask   isfloating	isterminal  noswallow	monitor */
 	{ "Gimp",						NULL,			NULL,				0,			1,			0, 			0, 			-1 },
-	{ "Firefox",					NULL,			NULL,				1 << 8,		0,			0, 			0, 			-1 },
 	{ NULL,							"spterm",		NULL,				SPTAG(0),	1,			0, 			0, 			-1 },
 	{ NULL,							"spfm",			NULL,				SPTAG(1),	1,			0, 			0, 			-1 },
 	{ NULL,							"keepassxc",	NULL,				SPTAG(2),	0,			0, 			0, 			-1 },
