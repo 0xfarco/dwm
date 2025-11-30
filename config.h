e164b05c2679465b660078de131fb22aa991ab5b
0xfarco <0xfarco@gmail.com>
2025-11-30 23:11:25 +0530

    change dmenu_run to dmenu_run_history

--- config.h
+++ config.h
@@ -87,7 +87,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, NULL };
+static const char *dmenucmd[] = { "dmenu_run_history", "-m", dmenumon, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 #include "movestack.c"
